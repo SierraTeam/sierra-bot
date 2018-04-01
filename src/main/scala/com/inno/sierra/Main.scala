612978ed767189f0a5040919cd98745fc55ff13e
Ilya V <i.voloshanovskiy@innopolis.ru>
2018-04-01 13:34:49 +0300

    init db in Main.scala

--- src/main/scala/com/inno/sierra/Main.scala
+++ src/main/scala/com/inno/sierra/Main.scala
@@ -1,10 +1,14 @@
 package com.inno.sierra
 
 import com.inno.sierra.bot.SierraBot
+import com.inno.sierra.model.DbSchema
 
 object Main {
 
   def main(args: Array[String]) {
+    // TODO: delete before production
+    DbSchema.init()
+    
     SierraBot.run()
   }
 }
