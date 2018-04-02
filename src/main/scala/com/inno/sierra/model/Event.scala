e0a1def6e86c63725d433c2f4cf97697ce158ace
Ilya V <i.voloshanovskiy@innopolis.ru>
2018-04-02 20:12:12 +0300

    merge

--- src/main/scala/com/inno/sierra/model/Event.scala
+++ src/main/scala/com/inno/sierra/model/Event.scala
@@ -2,6 +2,7 @@ package com.inno.sierra.model
 
 import org.squeryl.KeyedEntity
 import java.util.Date
+import scala.collection.mutable
 
 case class Event private (
             var id: Long,
@@ -22,5 +23,9 @@ object Event {
   def assignEventTo(eventId: Long, chatSessionId: Long): Unit = {
     val cse = new ChatSessionEvents(eventId, chatSessionId)
     DbSchema.insert(cse)
+  }
+
+  def get(ids: Option[mutable.Set[Long]]): mutable.Set[Event] = {
+    DbSchema.getAllEvents(ids)
   }
 }
