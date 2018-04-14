dc7ec890ae69eef0f81f75059329f70883aab766
Rafis Ganeev <r.ganeev@innopolis.ru>
2018-04-14 10:59:44 +0300

    Add prototype of calendar date picker

--- src/main/scala/com/inno/sierra/bot/SierraBot.scala
+++ src/main/scala/com/inno/sierra/bot/SierraBot.scala
@@ -1,17 +1,17 @@
 package com.inno.sierra.bot
 
 import java.text.SimpleDateFormat
-import java.util.Date
+import java.util.{Calendar, Date, GregorianCalendar}
 
 import com.inno.sierra.model.{ChatSession, ChatState, Event}
 import info.mukel.telegrambot4s.api._
-import info.mukel.telegrambot4s.api.declarative.Commands
+import info.mukel.telegrambot4s.api.declarative.{Callbacks, Commands}
 import info.mukel.telegrambot4s.methods.{GetMe, SendMessage}
 import info.mukel.telegrambot4s.models._
-import java.util.Calendar
 import java.util.concurrent.Executors
 
 import akka.actor.{ActorSystem, Cancellable, Props}
+import info.mukel.telegrambot4s.methods.EditMessageReplyMarkup
 
 import scala.concurrent.duration._
 import com.typesafe.config.ConfigFactory
@@ -20,7 +20,7 @@ import scala.collection.mutable.MutableList
 import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.util.{Failure, Success}
 
-abstract class SierraBot extends TelegramBot with Commands{
+abstract class SierraBot extends TelegramBot with Commands with Callbacks {
 //  lazy val botName = ConfigFactory.load().getString("bot.name")
   val botName: String =
     Await.result(request(GetMe).map(_.firstName), 10.seconds)
@@ -220,4 +220,146 @@ abstract class SierraBot extends TelegramBot with Commands{
       case Failure(e) => logger.error("Message sending error: ", e)
     }
   }
+
+  /**
+    * Chat message ID represents a reference to concrete message in some chat,
+    * so it is kinda unique identifier for a message.
+    *
+    * @param chatId
+    * @param messageId
+    */
+  case class ChatMessageId(chatId: Long, messageId: Int)
+
+  val currentShownDates: scala.collection.mutable.Map[ChatMessageId, (Int, Int)] = scala.collection.mutable.Map.empty[ChatMessageId, (Int, Int)]
+
+  val CALENDAR_DAY_TAG = "calendar-day"
+  def calendarDayTag(s: String): String = prefixTag(CALENDAR_DAY_TAG)(s)
+
+  onCallbackWithTag("ignore") { implicit cbq =>
+    // Notification only shown to the user who pressed the button.
+    ackCallback()
+  }
+
+  onCallbackWithTag(CALENDAR_DAY_TAG) { implicit cbq =>
+    // Notification only shown to the user who pressed the button.
+    ackCallback()
+
+    for {
+      data <- cbq.data
+      Extractors.Int(dayOfMonth) = data
+      msg <- cbq.message
+    } /* do */ {
+
+      val (year: Int, month: Int) = currentShownDates(ChatMessageId(msg.source, msg.messageId))
+
+      val dateStr = "%02d.%02d.%04d".format(dayOfMonth, month, year)
+
+      request(
+        SendMessage(
+          ChatId(msg.source),
+          dateStr
+        )
+      )
+    }
+  }
+
+  onCallbackWithTag("month-") { implicit cbq =>
+    // Notification only shown to the user who pressed the button.
+    ackCallback()
+
+    for {
+      data <- cbq.data
+      msg <- cbq.message
+    } /* do */ {
+
+      val (year: Int, month: Int) = currentShownDates(ChatMessageId(msg.source, msg.messageId))
+
+      val (yearNew: Int, monthNew: Int) = data match {
+        case "next" =>
+          month + 1 match {
+            case monthPlusOne if monthPlusOne > 12 => (year + 1, 1)
+            case _ => (year, month + 1)
+          }
+        case "previous" =>
+          month - 1 match {
+            case monthMinusOne if monthMinusOne < 1 => (year - 1, 1)
+            case _ => (year, month - 1)
+          }
+      }
+
+      currentShownDates(ChatMessageId(msg.chat.id, msg.messageId)) = (yearNew, monthNew)
+
+      val calendar = createCalendar(yearNew, monthNew)
+
+      request(
+        EditMessageReplyMarkup(
+          Some(ChatId(msg.source)), // msg.chat.id
+          Some(msg.messageId),
+          replyMarkup = Some(calendar)))
+    }
+  }
+
+  def createCalendar(year: Int, month: Int): InlineKeyboardMarkup = {
+    val daysInWeek = Calendar.SATURDAY
+    val daysInCalendarMax = 42
+    val calendar: MonthCalendar = MonthCalendar(month, year)
+
+    val header = Seq(
+      InlineKeyboardButton.callbackData(
+        s"${calendar.monthName} $year",
+        "ignore"
+      )
+    )
+    val daysOfWeek = List("M", "T", "W", "R", "F", "S", "U").map { dayOfWeek =>
+      InlineKeyboardButton.callbackData(
+        dayOfWeek,
+        "ignore"
+      )
+    }
+    val daySlotsInMonth: Seq[String] = (Seq().padTo(calendar.dayOfWeek, "  ") ++
+      calendar.daysInMonth.map(_.toString))
+      .padTo(daysInCalendarMax, "  ")
+    val body = daySlotsInMonth.grouped(daysInWeek).map { weekSlots =>
+      weekSlots.map { dayOfMonth =>
+        InlineKeyboardButton.callbackData(
+          dayOfMonth,
+          dayOfMonth match {
+            case " " => "ignore"
+            case _ => calendarDayTag(dayOfMonth)
+          }
+        )
+      }
+    } toSeq
+    val footer = Seq(
+      InlineKeyboardButton.callbackData("<", "month-previous"),
+      InlineKeyboardButton.callbackData(" ", "ignore"),
+      InlineKeyboardButton.callbackData(">", "month-next")
+    )
+
+    InlineKeyboardMarkup(
+      Seq(
+        header,
+        daysOfWeek
+      )
+      ++
+      body
+      ++
+      Seq(
+        footer
+      )
+    )
+  }
+
+  onCommand("/calendar") { implicit msg =>
+    val now = new GregorianCalendar
+    val year = now.get(Calendar.YEAR)
+    val month = now.get(Calendar.MONTH)
+    val calendar = createCalendar(year, month)
+
+    reply("Please choose the date to conduct an event on", replyMarkup = Some(calendar)).map { msg =>
+      currentShownDates(ChatMessageId(msg.chat.id, msg.messageId)) = (year, month)
+      msg
+    }
+  }
+
 }
