d7434a95aff73c01844bb94e09a780b6e20ee96d
Ilya V <i.voloshanovskiy@innopolis.ru>
2018-04-27 13:14:58 +0300

    fix

--- src/main/scala/com/inno/sierra/bot/NotifierService.scala
+++ src/main/scala/com/inno/sierra/bot/NotifierService.scala
@@ -20,11 +20,16 @@ class NotifierService ()(implicit ec: ExecutionContext) extends LazyLogging {
     * @param timeframe time period in which events to notify will be collected from db
     */
   def sendMessages(sender: ActorRef, timeframe: FiniteDuration): Future[Unit] = Future  {
-    val tillDate = new Date(new Date().getTime + timeframe.toMillis)
+    val today = new Date(new Date().getTime + timeframe.toMillis)
+    logger.debug("Today is: " + today)
+    logger.debug("Time coeficient: " + ConfigFactory.load().getInt("time.coeficient"))
     val c = Calendar.getInstance()
-    c.setTimeInMillis(tillDate.getTime)
-    c.set(Calendar.HOUR_OF_DAY, tillDate.getHours +
+    c.setTimeInMillis(today.getTime)
+    c.set(Calendar.HOUR_OF_DAY, today.getHours +
       ConfigFactory.load().getInt("time.coeficient"))
+
+    val tillDate = new Date(c.getTimeInMillis)
+    logger.debug("Till Date: " + tillDate)
 
     val beginDate = new Timestamp(c.getTimeInMillis)
     val events = DbSchema.getAllEventsTillDate(tillDate)
